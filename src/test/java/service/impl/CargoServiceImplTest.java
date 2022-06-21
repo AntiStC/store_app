2b3b87d12143ab2d9b4cb82aeb3a95a677369e5d
Soddik <sodd86@gmail.com>
2022-06-21 18:19:39 +0300

    test

--- src/test/java/service/impl/CargoServiceImplTest.java
+++ src/test/java/service/impl/CargoServiceImplTest.java
@@ -1,9 +1,23 @@
 package service.impl;
 
 
+import org.junit.Before;
 import org.junit.Test;
+import service.CargoService;
+import service.PersonDetailsService;
+import service.PersonService;
 
 class CargoServiceImplTest {
+    private CargoService cargoService;
+    private PersonService personService;
+    private PersonDetailsService personDetailsService;
+
+    @Before
+    public void setUp(){
+        personDetailsService = new PersonDetailsServiceImpl();
+        personService =new PersonServiceImpl(personDetailsService);
+        cargoService = new CargoServiceImpl(personService);
+    }
 
     @Test
     void create() {

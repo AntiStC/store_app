f5c329b9ee2967e8a70d6ab6a7aae234525f99ca
Soddik <sodd86@gmail.com>
2022-06-21 17:14:03 +0300

    + PersonServiceImpl.java

---
+++ src/main/java/service/impl/PersonServiceImpl.java
@@ -0,0 +1,27 @@
+package service.impl;
+
+import entity.Person;
+import entity.dto.PersonDTO;
+import service.PersonService;
+
+public class PersonServiceImpl implements PersonService {
+    @Override
+    public PersonDTO create(Person person) {
+        return null;
+    }
+
+    @Override
+    public PersonDTO read(Person person) {
+        return null;
+    }
+
+    @Override
+    public PersonDTO update(Person person) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Person person) {
+        return false;
+    }
+}

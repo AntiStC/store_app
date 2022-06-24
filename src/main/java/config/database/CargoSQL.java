17c276d6d2dcaaf839a54789cc4d72ba895eae84
AntiStC <94138193+AntiStC@users.noreply.github.com>
2022-06-24 11:29:47 +0300

    + CargoSQL.java

---
+++ src/main/java/config/database/CargoSQL.java
@@ -0,0 +1,24 @@
+package config.database;
+
+public enum CargoSQL {
+    GET("SELECT * FROM cargos WHERE id = (?)"),
+    GET_ALL("SELECT * FROM cargos"),
+    INSERT("INSERT INTO cargos(id, name, description, type, state, weight," +
+            " volume, createAt, modifiedAt, owner)" +
+            " VALUES(uuid_generate_v4(),(?),(?),(?)) RETURNING id"),
+    DELETE("DELETE FROM cargos WHERE id = (?)"),
+    DELETE_ALL("TRUNCATE cargo CASCADE;"),
+    UPDATE("UPDATE cargos SET name = (?), description = (?), type = (?)," +
+            " state = (?), weight = (?), volume = (?), createAt = (?), modifiedAt = (?)," +
+            " owner = (?) WHERE id = (?) RETURNING id");
+
+    public final String QUERY;
+
+    String getQUERY() {
+        return QUERY;
+    }
+
+    CargoSQL(String QUERY) {
+        this.QUERY = QUERY;
+    }
+}

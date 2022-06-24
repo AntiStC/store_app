ce656cc128f9c742d264c61b68b86b4fc5a8b7cf
AntiStC <94138193+AntiStC@users.noreply.github.com>
2022-06-24 16:49:08 +0300

    + create PersonDetailsDAO + edit CargoDAO

--- src/main/java/dao/PersonDetailsDAO.java
+++ src/main/java/dao/PersonDetailsDAO.java
@@ -2,7 +2,6 @@ package dao;
 
 import entity.PersonDetails;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface PersonDetailsDAO extends DAOService<PersonDetails, UUID>{
--- src/main/java/dao/impl/PersonDetailsDAOImpl.java
+++ src/main/java/dao/impl/PersonDetailsDAOImpl.java
@@ -1,32 +1,144 @@
 package dao.impl;
 
+import config.database.ConnectorDB;
+import config.database.PersonDetailsSQL;
 import dao.PersonDetailsDAO;
+import entity.PersonDetails;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-//raw use of parametrized
+import java.util.UUID;
+
+
 public class PersonDetailsDAOImpl implements PersonDetailsDAO {
+
+
     @Override
-    public List findByAll() {
-        return null;
+    public PersonDetails create(PersonDetails personDetails) {
+        try (Connection connection = ConnectorDB.getConnection();
+             PreparedStatement statement = connection.prepareStatement
+                     (PersonDetailsSQL.INSERT.QUERY, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            statement.setObject(1, personDetails.getId());
+            statement.setString(2, personDetails.getFirstName());
+            statement.setString(3, personDetails.getLastName());
+            statement.setInt(4, personDetails.getPassportNum());
+            statement.setString(5, personDetails.getAddress());
+            statement.execute();
+
+            ResultSet rs = statement.getGeneratedKeys();
+
+            if (rs.next()) {
+                personDetails.setId(rs.getObject("id", UUID.class));
+                return personDetails;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConnectorDB.closeConnection();
+        }
+        return personDetails;
     }
 
     @Override
-    public Object findEntityById(Object id) {
-        return null;
+    public PersonDetails findById(UUID id) {
+        PersonDetails personDetails = null;
+        try (Connection connection = ConnectorDB.getConnection();
+             PreparedStatement statement = connection.prepareStatement(PersonDetailsSQL.GET.QUERY)) {
+            statement.setObject(1, id);
+            ResultSet rs = statement.executeQuery();
+
+            if (rs.next()) {
+                personDetails = new PersonDetails();
+                personDetails.setId(rs.getObject("id", UUID.class));
+                personDetails.setFirstName(rs.getString("first_name"));
+                personDetails.setLastName(rs.getString("last_name"));
+                personDetails.setPassportNum(rs.getInt("passportNum"));
+                personDetails.setAddress(rs.getString("address"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConnectorDB.closeConnection();
+        }
+        return personDetails;
     }
 
     @Override
-    public boolean delete(Object o) {
-        return false;
+    public PersonDetails update(PersonDetails personDetails) {
+        try (Connection connection = ConnectorDB.getConnection();
+             PreparedStatement statement = connection.prepareStatement(PersonDetailsSQL.UPDATE.QUERY)) {
+            statement.setObject(1, personDetails.getId());
+            statement.setString(2, personDetails.getFirstName());
+            statement.setString(3, personDetails.getLastName());
+            statement.setInt(4, personDetails.getPassportNum());
+            statement.setString(5, personDetails.getAddress());
+            statement.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConnectorDB.closeConnection();
+        }
+        return personDetails;
     }
 
     @Override
-    public Object create(Object o) {
-        return null;
+    public List<PersonDetails> findAll() {
+        List<PersonDetails> personDetailsList = null;
+        try (Connection connection = ConnectorDB.getConnection();
+             PreparedStatement statement = connection.prepareStatement(PersonDetailsSQL.GET_ALL.QUERY)) {
+
+            ResultSet rs = statement.executeQuery();
+
+            personDetailsList = new ArrayList<>();
+
+            while (rs.next()) {
+                PersonDetails personDetails = new PersonDetails();
+                personDetails.setId(rs.getObject("id", UUID.class));
+                personDetails.setFirstName(rs.getString("name"));
+                personDetails.setLastName(rs.getString("description"));
+                personDetails.setPassportNum(rs.getInt("type"));
+                personDetails.setAddress(rs.getString("state"));
+
+                personDetailsList.add(personDetails);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConnectorDB.closeConnection();
+        }
+        return personDetailsList;
+    }
+
+
+    @Override
+    public void delete(UUID id) {
+        if (findById(id) != null) {
+            try (Connection connection = ConnectorDB.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(PersonDetailsSQL.DELETE.QUERY)) {
+                statement.setObject(1, id);
+                statement.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                ConnectorDB.closeConnection();
+            }
+        }
     }
 
     @Override
-    public Object update(Object o) {
-        return null;
+    public void deleteAll() {
+        try (Connection connection = ConnectorDB.getConnection();
+             PreparedStatement statement = connection.prepareStatement(PersonDetailsSQL.DELETE_ALL.QUERY)) {
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConnectorDB.closeConnection();
+        }
     }
 }

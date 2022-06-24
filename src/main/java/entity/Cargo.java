f2b47ea47736d841a1035e7f59e07d5e3922cec1
Soddik <sodd86@gmail.com>
2022-06-24 21:51:46 +0300

    +cargo builder

--- src/main/java/entity/Cargo.java
+++ src/main/java/entity/Cargo.java
@@ -22,6 +22,28 @@ public class Cargo {
     public Cargo() {
     }
 
+    private Cargo(UUID id,
+                  String name,
+                  String description,
+                  CargoType type,
+                  CargoState state,
+                  Double weight,
+                  Double volume,
+                  LocalDateTime createdAt,
+                  LocalDateTime modifiedAt,
+                  Person owner) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.state = state;
+        this.weight = weight;
+        this.volume = volume;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+        this.owner = owner;
+    }
+
     public UUID getId() {
         return id;
     }
@@ -135,5 +157,83 @@ public class Cargo {
                 ", modifiedAt=" + modifiedAt +
                 ", owner=" + owner +
                 '}';
+    }
+
+    public static class Builder {
+        private UUID id;
+        private String name;
+        private String description;
+        private CargoType type;
+        private CargoState state;
+        private Double weight;
+        private Double volume;
+        private LocalDateTime createdAt;
+        private LocalDateTime modifiedAt;
+
+        private Person owner;
+
+        public Builder setId(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder setType(CargoType type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder setState(CargoState state) {
+            this.state = state;
+            return this;
+        }
+
+        public Builder setWeight(Double weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public Builder setVolume(Double volume) {
+            this.volume = volume;
+            return this;
+        }
+
+        public Builder setCreatedAt(LocalDateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public Builder setModifiedAt(LocalDateTime modifiedAt) {
+            this.modifiedAt = modifiedAt;
+            return this;
+        }
+
+        public Builder setOwner(Person owner) {
+            this.owner = owner;
+            return this;
+        }
+
+        public Cargo build() {
+            return new Cargo(
+                    id,
+                    name,
+                    description,
+                    type,
+                    state,
+                    weight,
+                    volume,
+                    createdAt,
+                    modifiedAt,
+                    owner);
+        }
     }
 }

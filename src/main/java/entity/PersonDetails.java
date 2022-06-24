fb43bba943c804f88c1417e7a2d81a4fdd492ab9
Soddik <sodd86@gmail.com>
2022-06-24 21:52:02 +0300

    +persondetails builder

--- src/main/java/entity/PersonDetails.java
+++ src/main/java/entity/PersonDetails.java
@@ -13,6 +13,14 @@ public class PersonDetails {
     public PersonDetails() {
     }
 
+    private PersonDetails(UUID id, String firstName, String lastName, Integer passportNum, String address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passportNum = passportNum;
+        this.address = address;
+    }
+
     public UUID getId() {
         return id;
     }
@@ -81,5 +89,47 @@ public class PersonDetails {
                 ", passportNum=" + passportNum +
                 ", address='" + address + '\'' +
                 '}';
+    }
+
+    public static class Builder {
+        private UUID id;
+        private String firstName;
+        private String lastName;
+        private Integer passportNum;
+        private String address;
+
+        public Builder setId(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setPassportNum(Integer passportNum) {
+            this.passportNum = passportNum;
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public PersonDetails build() {
+            return new PersonDetails(
+                    id,
+                    firstName,
+                    lastName,
+                    passportNum,
+                    address);
+        }
     }
 }

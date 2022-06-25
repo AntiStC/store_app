aa4d77e9afef2f30f33c518f40a86fc7f73dfdeb
Soddik <sodd86@gmail.com>
2022-06-25 18:25:52 +0300

    +CargoDto - class +CargoDto Builder

---
+++ src/main/java/dto/CargoDto.java
@@ -0,0 +1,129 @@
+package dto;
+
+import entity.Person;
+import entity.enums.CargoState;
+import entity.enums.CargoType;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class CargoDto {
+    private final UUID id;
+    private final String name;
+    private final String description;
+    private final CargoType type;
+    private final CargoState state;
+    private final Double weight;
+    private final Double volume;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+
+    private Person owner;
+
+    private CargoDto(UUID id, String name, String description, CargoType type, CargoState state, Double weight, Double volume, LocalDateTime createdAt, LocalDateTime modifiedAt, Person owner) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.state = state;
+        this.weight = weight;
+        this.volume = volume;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "CargoDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", type=" + type +
+                ", state=" + state +
+                ", weight=" + weight +
+                ", volume=" + volume +
+                ", createdAt=" + createdAt +
+                ", modifiedAt=" + modifiedAt +
+                ", owner=" + owner +
+                '}';
+    }
+
+    public static class Builder {
+        private UUID id;
+        private String name;
+        private String description;
+        private CargoType type;
+        private CargoState state;
+        private Double weight;
+        private Double volume;
+        private LocalDateTime createdAt;
+        private LocalDateTime modifiedAt;
+
+        private Person owner;
+
+        public Builder setId(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder setType(CargoType type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder setState(CargoState state) {
+            this.state = state;
+            return this;
+        }
+
+        public Builder setWeight(Double weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public Builder setVolume(Double volume) {
+            this.volume = volume;
+            return this;
+        }
+
+        public Builder setCreatedAt(LocalDateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public Builder setModifiedAt(LocalDateTime modifiedAt) {
+            this.modifiedAt = modifiedAt;
+            return this;
+        }
+
+        public Builder setOwner(Person owner) {
+            this.owner = owner;
+            return this;
+        }
+
+        public CargoDto build() {
+            return new CargoDto(
+                    id,
+                    name,
+                    description,
+                    type,
+                    state,
+                    weight,
+                    volume,
+                    createdAt,
+                    modifiedAt,
+                    owner);
+        }
+    }
+}

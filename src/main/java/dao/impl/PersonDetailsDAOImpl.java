172e77ff2713446f84348c4622b2e97e8889a563
AntiStC <94138193+AntiStC@users.noreply.github.com>
2022-06-29 16:33:27 +0300

    + SQL person_details

--- src/main/java/dao/impl/PersonDetailsDAOImpl.java
+++ src/main/java/dao/impl/PersonDetailsDAOImpl.java
@@ -1,7 +1,6 @@
 package dao.impl;
 
 import config.database.ConnectorDB;
-import config.database.PersonDetailsSQL;
 import dao.PersonDetailsDAO;
 import model.entity.PersonDetails;
 
@@ -15,13 +14,21 @@ import java.util.UUID;
 
 
 public class PersonDetailsDAOImpl implements PersonDetailsDAO {
-
+    public static final String get = "SELECT * FROM person_details WHERE id = (?)";
+    public static final String getAll = "SELECT * FROM person_details";
+    public static final String insert = "INSERT INTO person_details(id, first_name, " +
+            "last_name, passport_num, address)" +
+            " VALUES(uuid_generate_v4(),(?),(?),(?)) RETURNING id";
+    public static final String delete = "DELETE FROM person_details WHERE id = (?)";
+    public static final String deleteAll = "TRUNCATE person_details CASCADE";
+    public static final String update = "UPDATE person_details SET first_name = (?), " +
+            "last_name = (?), passport_num = (?), address = (?) WHERE id = (?) RETURNING id";
 
     @Override
     public PersonDetails create(PersonDetails personDetails) {
         try (Connection connection = ConnectorDB.getConnection();
              PreparedStatement statement = connection.prepareStatement
-                     (PersonDetailsSQL.INSERT.QUERY, PreparedStatement.RETURN_GENERATED_KEYS)) {
+                     (insert, PreparedStatement.RETURN_GENERATED_KEYS)) {
             statement.setObject(1, personDetails.getId());
             statement.setString(2, personDetails.getFirstName());
             statement.setString(3, personDetails.getLastName());
@@ -47,7 +54,7 @@ public class PersonDetailsDAOImpl implements PersonDetailsDAO {
     public PersonDetails findById(UUID id) {
         PersonDetails personDetails = null;
         try (Connection connection = ConnectorDB.getConnection();
-             PreparedStatement statement = connection.prepareStatement(PersonDetailsSQL.GET.QUERY)) {
+             PreparedStatement statement = connection.prepareStatement(get)) {
             statement.setObject(1, id);
             ResultSet rs = statement.executeQuery();
 
@@ -70,7 +77,7 @@ public class PersonDetailsDAOImpl implements PersonDetailsDAO {
     @Override
     public PersonDetails update(PersonDetails personDetailsDto) {
         try (Connection connection = ConnectorDB.getConnection();
-             PreparedStatement statement = connection.prepareStatement(PersonDetailsSQL.UPDATE.QUERY)) {
+             PreparedStatement statement = connection.prepareStatement(update)) {
             statement.setObject(1, personDetailsDto.getId());
             statement.setString(2, personDetailsDto.getFirstName());
             statement.setString(3, personDetailsDto.getLastName());
@@ -90,7 +97,7 @@ public class PersonDetailsDAOImpl implements PersonDetailsDAO {
     public List<PersonDetails> findAll() {
         List<PersonDetails> personDetailsList = null;
         try (Connection connection = ConnectorDB.getConnection();
-             PreparedStatement statement = connection.prepareStatement(PersonDetailsSQL.GET_ALL.QUERY)) {
+             PreparedStatement statement = connection.prepareStatement(getAll)) {
 
             ResultSet rs = statement.executeQuery();
 
@@ -119,7 +126,7 @@ public class PersonDetailsDAOImpl implements PersonDetailsDAO {
     public void delete(UUID id) {
         if (findById(id) != null) {
             try (Connection connection = ConnectorDB.getConnection();
-                 PreparedStatement statement = connection.prepareStatement(PersonDetailsSQL.DELETE.QUERY)) {
+                 PreparedStatement statement = connection.prepareStatement(delete)) {
                 statement.setObject(1, id);
                 statement.execute();
             } catch (SQLException e) {
@@ -133,7 +140,7 @@ public class PersonDetailsDAOImpl implements PersonDetailsDAO {
     @Override
     public void deleteAll() {
         try (Connection connection = ConnectorDB.getConnection();
-             PreparedStatement statement = connection.prepareStatement(PersonDetailsSQL.DELETE_ALL.QUERY)) {
+             PreparedStatement statement = connection.prepareStatement(deleteAll)) {
             statement.execute();
         } catch (SQLException e) {
             e.printStackTrace();

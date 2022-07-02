51e60d940d69469a411cf22b2a3e278071f96fef
Soddik <sodd86@gmail.com>
2022-07-02 09:01:05 +0300

    fix: unnecessary interfaces removed

--- src/main/java/service/PersonDetailsService.java
+++ src/main/java/service/PersonDetailsService.java
@@ -1,7 +1,47 @@
 package service;
 
+import dao.PersonDetailDAO;
+import mapper.PersonDetailsMapper;
 import model.dto.PersonDetailsDto;
 
-public interface PersonDetailsService extends CrudService<PersonDetailsDto> {
 
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class PersonDetailsService {
+    private final PersonDetailDAO personDetailDAO;
+
+    public PersonDetailsService(PersonDetailDAO personDetailDAO) {
+        this.personDetailDAO = personDetailDAO;
+    }
+
+
+    public PersonDetailsDto create(PersonDetailsDto personDetailsDto) {
+        return PersonDetailsMapper.toDto(personDetailDAO.create(PersonDetailsMapper.toEntity(personDetailsDto)));
+    }
+
+
+    public PersonDetailsDto read(UUID id) {
+        return PersonDetailsMapper.toDto(personDetailDAO.findById(id));
+    }
+
+
+    public List<PersonDetailsDto> getAll() {
+        return personDetailDAO.findAll().stream()
+                .map(PersonDetailsMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    public PersonDetailsDto update(PersonDetailsDto personDetailsDto) {
+        return PersonDetailsMapper.toDto(personDetailDAO.update(PersonDetailsMapper.toEntity(personDetailsDto)));
+    }
+
+    public void delete(UUID id) {
+        personDetailDAO.delete(id);
+    }
+
+    public void deleteAll() {
+        personDetailDAO.deleteAll();
+    }
 }

4e78a23fcb30de84fa831b0567b3fbc79cd936ce
Soddik <sodd86@gmail.com>
2022-07-03 00:57:05 +0300

    fix(DAO)!: add todos fix(DAO)!: refactoring

--- src/main/java/dao/impl/PersonDetailDAOImpl.java
+++ src/main/java/dao/impl/PersonDetailDAOImpl.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 
 public class PersonDetailDAOImpl implements PersonDetailDAO {
 
-    private void statement(PersonDetails personDetails, PreparedStatement statement)
+    private void executeStatement(PersonDetails personDetails, PreparedStatement statement)
             throws SQLException {
         statement.setObject(1, personDetails.getId());
         statement.setString(2, personDetails.getFirstName());
@@ -26,13 +26,23 @@ public class PersonDetailDAOImpl implements PersonDetailDAO {
         statement.execute();
     }
 
+    private PersonDetails createPersonDetails(ResultSet rs) throws SQLException {
+        return new PersonDetails.Builder()
+                .setId(rs.getObject("id", UUID.class))
+                .setFirstName(rs.getString("first_name"))
+                .setLastName(rs.getString("last_name"))
+                .setPassportNum(rs.getInt("passport_num"))
+                .setAddress(rs.getString("address"))
+                .build();
+    }
+
     @Override
     public PersonDetails create(PersonDetails personDetails) {
         try (Connection connection = ConnectorDB.getConnection();
              PreparedStatement statement = connection.prepareStatement
                      (PersonDetailSql.SQL_QUERY_PERSON_DETAIL_INSERT,
                              PreparedStatement.RETURN_GENERATED_KEYS)) {
-            statement(personDetails, statement);
+            executeStatement(personDetails, statement);
 
             ResultSet rs = statement.getGeneratedKeys();
 
@@ -59,12 +69,7 @@ public class PersonDetailDAOImpl implements PersonDetailDAO {
             ResultSet rs = statement.executeQuery();
 
             if (rs.next()) {
-                personDetails = new PersonDetails();
-                personDetails.setId(rs.getObject("id", UUID.class));
-                personDetails.setFirstName(rs.getString("first_name"));
-                personDetails.setLastName(rs.getString("last_name"));
-                personDetails.setPassportNum(rs.getInt("passport_num"));
-                personDetails.setAddress(rs.getString("address"));
+                personDetails = createPersonDetails(rs);
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -79,7 +84,7 @@ public class PersonDetailDAOImpl implements PersonDetailDAO {
         try (Connection connection = ConnectorDB.getConnection();
              PreparedStatement statement = connection.prepareStatement
                      (PersonDetailSql.SQL_QUERY_PERSON_DETAIL_UPDATE)) {
-            statement(personDetailsDto, statement);
+            executeStatement(personDetailsDto, statement);
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -101,14 +106,7 @@ public class PersonDetailDAOImpl implements PersonDetailDAO {
             personDetailsList = new ArrayList<>();
 
             while (rs.next()) {
-                PersonDetails personDetailsDto = new PersonDetails();
-                personDetailsDto.setId(rs.getObject("id", UUID.class));
-                personDetailsDto.setFirstName(rs.getString("name"));
-                personDetailsDto.setLastName(rs.getString("description"));
-                personDetailsDto.setPassportNum(rs.getInt("type"));
-                personDetailsDto.setAddress(rs.getString("state"));
-
-                personDetailsList.add(personDetailsDto);
+                personDetailsList.add(createPersonDetails(rs));
             }
         } catch (SQLException e) {
             e.printStackTrace();

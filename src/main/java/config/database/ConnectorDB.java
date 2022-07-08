26f1dbaf16e1699627dfef26bd036340527c867b
AntiStC <94138193+AntiStC@users.noreply.github.com>
2022-07-08 12:18:54 +0300

    + edit ConnectorDB

--- src/main/java/config/database/ConnectorDB.java
+++ src/main/java/config/database/ConnectorDB.java
@@ -6,11 +6,9 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class ConnectorDB {
-
-
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        ResourceBundle resource = ResourceBundle.getBundle("database");
         Class.forName("db.driver");
-        ResourceBundle resource = ResourceBundle.getBundle("store_app");
         String url = resource.getString("db.url");
         String user = resource.getString("db.user");
         String pass = resource.getString("db.password");

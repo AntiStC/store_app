1caeff5f8e930a1851b5f15ee0d6627c894353f0
Soddik <sodd86@gmail.com>
2022-07-09 11:02:15 +0300

    fix: works correctly ???

--- src/main/java/controller/PersonController.java
+++ src/main/java/controller/PersonController.java
@@ -1,68 +1,48 @@
 package controller;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import dao.impl.CargoDAOImpl;
-import dao.impl.PersonDAOImpl;
-import dao.impl.PersonDetailDAOImpl;
-import model.dto.PersonDto;
-import service.CargoService;
-import service.PersonDetailsService;
 import service.PersonService;
+import util.query.Parser;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.UUID;
-
-import static java.lang.System.out;
 
 @WebServlet(name = "PersonController", value = "/persons/*")
 public class PersonController extends HttpServlet {
     private PersonService personService;
 
     @Override
+    public void init() throws ServletException {
+        super.init();
+        personService = new PersonService();
+    }
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        personService = new PersonService(new PersonDAOImpl(), new PersonDetailsService(new PersonDetailDAOImpl()), new CargoService(new CargoDAOImpl()));
-        PrintWriter pw = response.getWriter();
+        request.setCharacterEncoding("UTF8");
 
-        try {
-            pw.println("getRequestURI: " + getIdFromPath(request.getRequestURI()));
-        } catch (IllegalArgumentException e) {
-            pw.println("ID not found! \n" + e.getMessage());
-        } finally {
-            pw.flush();
-            pw.close();
+        try (PrintWriter pw = response.getWriter()) {
+            UUID id = Parser.getIdFromPath(request.getRequestURI());
+            if (id != null) {
+                pw.println(personService.read(id));
+            } else {
+                pw.println(personService.getAll());
+            }
         }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        personService = new PersonService(new PersonDAOImpl(), new PersonDetailsService(new PersonDetailDAOImpl()), new CargoService(new CargoDAOImpl()));
-        out.println("POST");
-
-        BufferedReader reader = request.getReader();
-        JsonObject object = JsonParser.parseReader(reader).getAsJsonObject();
-
-        PersonDto personDto = new PersonDto.Builder()
-                .setLogin(object.get("login").getAsString())
-                .setPassword(object.get("password").getAsString())
-                .build();
-        out.println(personDto);
-
+        // TODO: 09.07.2022 implement saveOrUpdate() ???
+        personService.create(Parser.parseEntry(request.getReader()));
     }
 
-    private UUID getIdFromPath(String path) {
-        String[] split = path.split("/");
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        if (split.length > 0) {
-            return UUID.fromString(split[split.length - 1]);
-        } else {
-            throw new IllegalArgumentException();
-        }
     }
 }

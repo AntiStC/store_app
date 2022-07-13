787e684d6f3cd035f9e6f65cc5561e0279a3bf53
AntiStC <94138193+AntiStC@users.noreply.github.com>
2022-07-13 17:36:02 +0300

    + edit PersonDetailSql

--- src/main/java/util/query/PersonDetailSql.java
+++ src/main/java/util/query/PersonDetailSql.java
@@ -9,37 +9,37 @@ public class PersonDetailSql {
     public static final String SQL_QUERY_PERSON_DETAIL_GET =
             """
                     SELECT id,
-                    first_name,
-                    last_name,
-                    passport_num,
-                    address
+                           first_name,
+                           last_name,
+                           passport_num,
+                           address
                     FROM person_detail
                     WHERE id = (?)
                     """;
     public static final String SQL_QUERY_PERSON_DETAIL_GET_ALL =
             """
                     SELECT id,
-                    first_name,
-                    last_name,
-                    passport_num,
-                    address
+                           first_name,
+                           last_name,
+                           passport_num,
+                           address
                     FROM person_detail
                     """;
     public static final String SQL_QUERY_PERSON_DETAIL_INSERT =
             """
                     INSERT INTO person_detail(
-                    id,
-                    first_name,
-                    last_name,
-                    passport_num,
-                    address)
-                    VALUES(uuid_generate_v4(),(?),(?),(?))
+                                id,
+                                first_name,
+                                last_name,
+                                passport_num,
+                                address)
+                    VALUES(uuid_generate_v4(),(?),(?),(?),(?))
                     RETURNING id
                     """;
     public static final String SQL_QUERY_PERSON_DETAIL_DELETE =
             """
-                    DELETE FROM
-                    person_detail
+                    DELETE 
+                    FROM person_detail
                     WHERE id = (?)
                     """;
     public static final String SQL_QUERY_PERSON_DETAIL_DELETE_ALL =
@@ -49,12 +49,12 @@ public class PersonDetailSql {
     public static final String SQL_QUERY_PERSON_DETAIL_UPDATE =
             """
                     UPDATE
-                    person_detail
-                    SET first_name = (?),
-                    last_name = (?),
-                    passport_num = (?),
-                    address = (?)
-                    WHERE id = (?)
+                        person_detail
+                    SET first_name      = (?),
+                        last_name       = (?),
+                        passport_num    = (?),
+                        address         = (?)
+                    WHERE id            = (?)
                     RETURNING id
                     """;
 }

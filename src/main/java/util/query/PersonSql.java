3ea799790e90267f42bddb60f4d3933c4b071de3
AntiStC <94138193+AntiStC@users.noreply.github.com>
2022-07-13 17:31:46 +0300

    + edit PersonSql

--- src/main/java/util/query/PersonSql.java
+++ src/main/java/util/query/PersonSql.java
@@ -9,21 +9,25 @@ public class PersonSql {
     public static final String SQL_QUERY_PERSON_GET =
             """
                     SELECT id,
-                    login,
-                    password
-                    FROM person 
-                    INNER JOIN person_detail pd 
-                    on person.id = pd.person_fk
-                    WHERE id = (?) 
+                           login,
+                           password
+                    FROM person
+                            INNER JOIN person_detail pd
+                                       ON person.person_detail_fk = pd.person_detail_id
+                            INNER JOIN cargo_list cl
+                                       ON person.person_id = cl.person_list_fk
+                    WHERE id = (?)
                     """;
     public static final String SQL_QUERY_PERSON_GET_ALL =
             """
-                    SELECT id,
-                    login,
-                    password
-                    FROM person 
-                    INNER JOIN person_detail pd 
-                    on person.id = pd.person_fk
+                   SELECT id,
+                           login,
+                           password
+                    FROM person
+                            INNER JOIN person_detail pd
+                                       ON person.person_detail_fk = pd.person_detail_id
+                            INNER JOIN cargo_list cl
+                                       ON person.person_id = cl.person_list_fk
                     """;
     public static final String SQL_QUERY_PERSON_INSERT =
             """
@@ -31,7 +35,7 @@ public class PersonSql {
                     id,
                     login,
                     password)
-                    VALUES(uuid_generate_v4(),(?),(?),(?))
+                    VALUES(uuid_generate_v4(),(?),(?))
                     RETURNING id
                     """;
     public static final String SQL_QUERY_PERSON_DELETE =

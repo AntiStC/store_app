83ea28f5ea125bf9bf9c094049fe58c0736e8d09
AntiStC <94138193+AntiStC@users.noreply.github.com>
2022-07-27 10:46:43 +0300

    fixup! sql create table

--- src/resources/sql/postgresql/create-tables.sql
+++ src/resources/sql/postgresql/create-tables.sql
@@ -1,32 +1,46 @@
+CREATE TABLE IF NOT EXISTS person_detail
+(
+    person_detail_id UUID PRIMARY KEY,
+    first_name       VARCHAR,
+    last_name        VARCHAR,
+    passport_num     BIGINT,
+    address          VARCHAR
+);
+
 CREATE TABLE IF NOT EXISTS person
 (
-    id          UUID PRIMARY KEY,
-    login       VARCHAR UNIQUE NOT NULL,
-    password    VARCHAR UNIQUE NOT NULL
+    person_id        UUID PRIMARY KEY,
+    login            VARCHAR UNIQUE NOT NULL,
+    password         VARCHAR UNIQUE NOT NULL,
+    person_detail_fk UUID
 );
 
 CREATE TABLE IF NOT EXISTS cargo
 (
-    id           UUID PRIMARY KEY,
-    name         VARCHAR,
-    description  VARCHAR,
-    type         VARCHAR NOT NULL,
-    state        VARCHAR NOT NULL,
-    weight       DOUBLE PRECISION NOT NULL,
-    volume       DOUBLE PRECISION NOT NULL,
-    create_at    TIMESTAMP NOT NULL DEFAULT NOW(),
-    modified_at  TIMESTAMP,
-    person_fk    UUID,
-    FOREIGN KEY (person_fk) REFERENCES person (id)
+    cargo_id    UUID PRIMARY KEY,
+    name        VARCHAR,
+    description VARCHAR,
+    type        VARCHAR          NOT NULL,
+    state       VARCHAR          NOT NULL,
+    weight      DOUBLE PRECISION NOT NULL,
+    volume      DOUBLE PRECISION NOT NULL,
+    create_at   TIMESTAMP        NOT NULL DEFAULT NOW(),
+    modified_at TIMESTAMP,
+    person_fk   UUID
 );
 
-CREATE TABLE IF NOT EXISTS person_detail
+CREATE TABLE IF NOT EXISTS cargo_list
 (
-    id           UUID PRIMARY KEY,
-    first_name   VARCHAR,
-    last_name    VARCHAR,
-    passport_num BIGINT,
-    address      VARCHAR,
-    person_fk    UUID,
-    FOREIGN KEY (person_fk) REFERENCES person (id)
+    cargo_list_fk  UUID UNIQUE NOT NULL,
+    person_list_fk UUID UNIQUE NOT NULL
 );
+
+ALTER TABLE cargo_list
+    ADD FOREIGN KEY (person_list_fk) REFERENCES person (person_id),
+    ADD FOREIGN KEY (cargo_list_fk) REFERENCES cargo (cargo_id);
+
+ALTER TABLE person
+    ADD FOREIGN KEY (person_detail_fk) REFERENCES person_detail (person_detail_id);
+
+ALTER TABLE cargo
+    ADD FOREIGN KEY (person_fk) REFERENCES cargo_list (person_list_fk);
